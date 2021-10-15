83f1d1b8c2bad66f5daff5f16d43f4fa29ae7eab
JaniraFV <alufav700@ieselcaminas.org>
2021-10-15 10:12:11 +0200

    v2 15/10

---
+++ utils/utils.php
@@ -0,0 +1,22 @@
+<?php
+function esOpcionMenuActiva(string $option): bool
+{
+    if (strpos($_SERVER["REQUEST_URI"], "/".$option) === 0){
+        return true;
+    }elseif ($_SERVER["REQUEST_URI"] == '/' && $option == 'index'){
+        return true;    
+    }else
+        return false;
+}
+
+function  existeOpcionMenuActivaEnArray(array $options): bool
+{
+
+    foreach ($options as $option){
+        if (esOpcionMenuActiva($option)){
+            return true;
+        }else
+        return false;
+
+    }
+}
