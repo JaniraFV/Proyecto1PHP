79ec47f4dc9dad6f6b80ec9cc165b28d61be61b0
JaniraFV <alufav700@ieselcaminas.org>
2021-11-24 11:31:47 +0100

    v8 register.view

---
+++ repository/UsuarioRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../entity/Usuario.php';
+
+require_once __DIR__ . '/../database/QueryBuilder.php';
+
+class UsuarioRepository extends QueryBuilder
+
+{
+
+    public function __construct(){
+
+        parent::__construct('users', 'Usuario');
+
+    }
+
+
+   
+
+}
